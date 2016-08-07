917a462a93fe9b418d38d3814b1c9aa1aad2962d
Tipunov Sergey <tipunovs@gmail.com>
2016-08-07 16:12:28 +0300

    task2

--- GoIT/js/1-2/js/2.js
+++ GoIT/js/1-2/js/2.js
@@ -1,3 +1,24 @@
-/**
- * Created by tipun on 23.07.2016.
- */
+var userNames = [];
+
+
+    for (var i = 0; i <5 ; i++) {
+        var name = prompt('Введите имена пользователей (5шт.)', '');
+        userNames.push(name);
+
+
+    }
+
+console.log(userNames)
+
+var login = prompt('Введите логин', '');
+console.log(login)
+function searchUser (login) {
+
+    for (var i = 0; i < userNames.length; i++)
+        if (userNames[i] == login) {
+        return alert("login', вы успешно вошли");
+    }
+
+    return alert("Пользователя' login' не существует");
+
+}
